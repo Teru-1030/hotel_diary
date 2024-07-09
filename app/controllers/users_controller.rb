774718056fd0676f19fa03b52e494f18e53c30b7
Teru-1030 <166922869+Teru-1030@users.noreply.github.com>
2024-07-09 14:46:51 +0000

    [Update]Usersコントローラー

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,42 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_parmas)
+    redirect_to @user
+  end
+  
+  private
+
+  def user_parmas
+    params.require(:user).permit(:name, :profile_image)
+  end
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
+    end
+  end
+  
+  private
+  
+  def user_state
+    user = Customer.find_by(email: params[:customer][:email])
+  return if user.nil?
+  return unless user.valid_password?(params[:customer][:password])
+  if is_active
+    userr_state() 
+  else
+    redirect_to signup_path 
+  end
+
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
-  get 'likes/index'
-  get 'comments/index'
-  get 'comments/show'
-  get 'tags/index'
-  get 'tags/show'
-  get 'posts/new'
-  get 'posts/index'
-  get 'posts/show'
-  get 'posts/edit'
   devise_for :users
   root to: "homes#top"
+  resources :users, only: [:show, :edit]
+  resources :posts, only: [:new, :index, :show, :edit]
+  resources :tags, only: [:index, :show]
+  resources :comments, only: [:index, :show]
+  resources :likes, only: [:index]
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
