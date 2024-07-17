ee8f47f938a0e81aff4120cba2cb1a0fcd90514a
EC2 Default User <ec2-user@ip-172-31-10-10.ap-northeast-1.compute.internal>
2024-07-17 14:47:40 +0000

    [Update]フラッシュメッセージ

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,12 @@ class SessionsController < ApplicationController
   protected
 
   def reject_user
+    byebug
     @user = User.find_by(email: params[:user][:email].downcase)
     if @user
       if (@user.valid_password?(params[:user][:password]) && (@user.active_for_authentication? == true))
         flash[:notice] = "退会済みです"
-        redirect_to new_user_session_path
+        redirect_to root_path
       else
       flash[:notice] = "項目を入力してください"
       end
