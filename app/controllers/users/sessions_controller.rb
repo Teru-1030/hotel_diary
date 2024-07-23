b5ca12e91b32b1e6e2614410b2fca18c8dc005a8
EC2 Default User <ec2-user@ip-172-31-10-10.ap-northeast-1.compute.internal>
2024-07-23 11:54:09 +0000

    update

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,17 +4,17 @@ class Users::SessionsController < Devise::SessionsController
  def guest_sign_in
     user = User.guest
     sign_in user
-    redirect_to user_path(user), notice: "guestuserでログインしました。"
+    redirect_to about_path, notice: "guestuserでログインしました。"
  end
   
   protected
 
   def reject_user
-    @user = User.find_by(email: params[:user][:email].downcase)
+    @user = User.find_by(email: params[:user][:email])
     if @user
-      if (@user.valid_password?(params[:user][:password]) && (@user.active_for_authentication? == true))
+      if @user.valid_password?(params[:user][:password]) && (@user.active_for_authentication? == false)
         flash[:notice] = "退会済みです"
-        redirect_to new_user_registration
+        redirect_to root_path
       else
       flash[:notice] = "項目を入力してください"
       end
