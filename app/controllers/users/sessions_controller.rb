5ebecb92ddc749095d7c0bf8d7f28e5ba034663c
EC2 Default User <ec2-user@ip-172-31-10-10.ap-northeast-1.compute.internal>
2024-07-24 14:40:18 +0000

    コントローラー修正

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class Users::SessionsController < Devise::SessionsController
-    
+    before_action :reject_user, only: [:create]
 
  def guest_sign_in
     user = User.guest
@@ -10,6 +12,7 @@ class Users::SessionsController < Devise::SessionsController
   protected
 
   def reject_user
+    byebug
     @user = User.find_by(email: params[:user][:email])
     if @user
       if @user.valid_password?(params[:user][:password]) && (@user.active_for_authentication? == false)
@@ -22,4 +25,27 @@ class Users::SessionsController < Devise::SessionsController
       flash[:notice] = "該当するユーザーが見つかりません"
     end
   end
+  # before_action :configure_sign_in_params, only: [:create]
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
 end
