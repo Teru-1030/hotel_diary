cbd7ca71c3db71b17e7c468a06e9bcbfed7b99ef
EC2 Default User <ec2-user@ip-172-31-10-10.ap-northeast-1.compute.internal>
2024-07-26 11:59:54 +0000

    [Add]非ログインユーザーアクセス権限

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
-  before_action :is_matching_login_user, only: [:edit, :update]
+  before_action :authenticate_user, only: [:edit, :update]
+  before_action :is_matching_login_user, only: [:edit, :update, :withdraw]
   before_action :ensure_guest_user, only: [:edit]
+  
   
   def show
     @user = User.find(params[:id])
@@ -12,7 +14,6 @@ class UsersController < ApplicationController
   end
   
   def update
-     is_matching_login_user
     @user = User.find(params[:id])
     if @user.update(user_parmas)
      flash[:notice] = "ユーザーの更新が完了しました"
@@ -23,7 +24,6 @@ class UsersController < ApplicationController
   end
   
   def withdraw
-    is_matching_login_user
     @user = User.find(params[:id])
     @user.update(is_active: false)
     reset_session
@@ -39,11 +39,19 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :profile_image, :self_introduction)
   end
   
+  
+  def authenticate_user
+    @current_user=User.find_by(id: session[:user_id])
+    if @current_user==nil
+      redirect_to new_user_session_path
+    end
+  end
+
   def is_matching_login_user
     user = User.find(params[:id])
     unless user.id == current_user.id
       redirect_to user_path(current_user.id)
-    end
+    end 
   end
   
   
