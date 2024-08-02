76191c5def0da958ab2161e170d5d85ac5204a4e
Teru-1030 <teru.1112222@gmail.com>
2024-08-02 13:42:25 +0000

    [Update]管理者機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,34 @@
 Rails.application.routes.draw do
   
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+  
+  scope module: :public do
+    root to: "homes#top"
+  
+    get 'homes/about' => 'homes#about', as: 'about'
+  
+    devise_scope :user do
+      post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+    end
+  
+    resources :users, only: [:show, :edit, :update, :withdraw, :index]
+  
+    resources :posts, only: [:new, :index, :show, :edit, :create, :destroy, :update] do
+      resources :comments, only: [:create, :destroy]
+    end
+  
+    resources :tags, only: [:index, :show]
+  
+    resources :likes, only: [:index]
+  
+    patch  '/users/:id/withdraw' => 'users#withdraw', as: 'withdraw'
+  
+    get "/search", to: "searches#search"
+  
+  end
+  
   devise_for :admin, skip: [:registrations], controllers: {
     sessions: 'admin/sessions'
   }
@@ -9,32 +38,5 @@ Rails.application.routes.draw do
     resources :users, only: [:destroy]
   end
   
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
-  }
-  
-  root to: "homes#top"
-  
-  get 'homes/about' => 'homes#about', as: 'about'
-  
-  devise_scope :user do
-    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
-  end
-  
-  resources :users, only: [:show, :edit, :update, :withdraw, :index]
-  
-  resources :posts, only: [:new, :index, :show, :edit, :create, :destroy, :update] do
-    resources :comments, only: [:create, :destroy]
-  end
-  
-  resources :tags, only: [:index, :show]
-  
-  resources :likes, only: [:index]
-  
-  patch  '/users/:id/withdraw' => 'users#withdraw', as: 'withdraw'
-  
-  get "/search", to: "searches#search"
-  
-   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
