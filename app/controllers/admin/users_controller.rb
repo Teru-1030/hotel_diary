f30b78043065d03b20b4e7fd9ce641916f906356
Teru-1030 <teru.1112222@gmail.com>
2024-08-05 14:34:41 +0000

    [Add]投稿管理者機能

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,5 @@
 class Admin::UsersController < ApplicationController
-  #before_action :authenticate_admin!
+   before_action :authenticate_admin!
    before_action :ensure_guest_user, only: [:show]
   def show
     @user = User.find(params[:id])
