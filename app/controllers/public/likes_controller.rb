891f95703ae25031a881b9494b5349990872e488
Teru-1030 <teru.1112222@gmail.com>
2024-08-20 13:48:33 +0000

    [Update]ゲストユーザのアクセス権限

--- app/controllers/public/likes_controller.rb
+++ app/controllers/public/likes_controller.rb
@@ -1,4 +1,5 @@
 class Public::LikesController < ApplicationController
+  before_action :ensure_guest_user, only: [:create, :destroy]
   
   def create
     post = Post.find(params[:post_id])
@@ -13,5 +14,13 @@ class Public::LikesController < ApplicationController
     like.destroy
     redirect_to request.referer
   end
+  
+  private
+  
+  def ensure_guest_user
+    if current_user.guest_user?
+      redirect_to request.referer, notice: "ゲストユーザーはいいねできません。"
+    end
+  end    
 
 end
